132a8a3b0fd8dbee964aa43837448bc57a68c8bf
Povlean <104959958+Povlean@users.noreply.github.com>
2023-05-26 16:14:03 +0800

    Delete BigJobApplication directory

--- BigJobApplication/app/src/main/java/com/example/bigjobapplication/constants/Constant.java
+++
@@ -1,5 +0,0 @@
-package com.example.bigjobapplication.constants;
-
-public interface Constant {
-    String IP_ADDRESS = "192.168.31.84:8082";
-}
--- BigJobApplication/app/src/main/java/com/example/bigjobapplication/utils/Constant.java
+++
@@ -1,7 +0,0 @@
-package com.example.bigjobapplication.utils;
-
-public class Constant {
-    public static final String DATABASE_NAME = "solar_term.db";
-    public static final int VERSION_CODE = 1;
-    public static final String TABLE_NAME = "tbl_solar";
-}
